950f24e015e982de6efa7beac5065022042bee0a
Staicu Adrian <adrianstacu888@gmail.com>
2020-02-11 21:16:59 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = new int[] { 2, 5, 7, 9, -5, 10, 12 };
+
+            for(int i = 0; i<array.Length; i++)
+            {
+                if (array[i] < 0)
+                {
+                    Console.WriteLine("Indexul negativ se afla pe pozitia " + i);
+                }
+            }
+
+        }
+    }
+}
